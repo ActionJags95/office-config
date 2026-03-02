614e7934e5c478e80e45b84766412a78cb090866
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2026-03-02 12:28:51 +0530

    Added description for code actions

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -61,6 +61,7 @@ return {
     })
 
     vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-    vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action,
+      { noremap = true, silent = true, desc = "Show code actions" })
   end,
 }
